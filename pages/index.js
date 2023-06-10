807220a524daaacf48f5118764e92c1cd38219f2
Smriti Goel <smritigoel93@gmail.com>
2023-06-10 23:21:14 +0530

    added structure

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
+import { useContext } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { useContext } from 'react'
-import { BaseContext } from '@/src/context/BaseContext'
+
+import { BaseContext } from 'src/context/BaseContext'
+import Sample from 'src/containers/Sample'
+
 export default function Home() {
   const { connect, connecting, account, disconnect, wallet } =
     useContext(BaseContext)
 
   return (
     <>
+      <Sample />
       <div>Hi</div>
       <div>Connecting: {connecting.toString()}</div>
       <div>Wallet: {account}</div>
---
+++ src/queries/index.js
@@ -0,0 +1,14 @@
+export const useGetTokenUSDPrices = (coinGeckoIds) => {
+  return useQuery(coinGeckoIds, async () => {
+    const config = {
+      headers: {
+        accept: 'application/json',
+      },
+      params: { ids: coinGeckoIds.join(','), vs_currencies: 'usd' },
+    }
+    return await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price',
+      config,
+    )
+  })
+}
