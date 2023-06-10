3776c1fcc4dfb3f878c251d1b2c6720cac2bf6a5
Alien Coder <paidcodecamp@gmail.com>
2023-06-10 23:24:09 +0530

    added: Mainnet support

--- src/utils/supportedChains.js
+++ src/utils/supportedChains.js
@@ -1,6 +1,12 @@
-const INFURA_KEY = 'ea28e9b13b404c2eb037c6c3264345f0'
+const INFURA_KEY = process.env.NEXT_PUBLIC_INFURA_KEY
 
 export const chains = [
+  {
+    id: '0x1',
+    token: 'ETH',
+    label: 'Ethereum',
+    rpcUrl: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+  },
   {
     id: '0x5',
     token: 'gETH',
