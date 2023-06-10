4e74626d5b9d2c8ffe410ad7e7872dd108b06221
Alien Coder <paidcodecamp@gmail.com>
2023-06-10 21:16:06 +0530

    added: react-query, moment

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
