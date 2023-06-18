e306172f96e8e402ab09e04cbe5a078f2a6904d3
Smriti Goel <smritigoel93@gmail.com>
2023-06-18 14:40:04 +0530

    added blockies

--- src/components/NameAddress/index.js
+++ src/components/NameAddress/index.js
@@ -1,12 +1,21 @@
 import { minifyAddress } from 'src/utils/web3Utils'
 import Typography from 'src/components/Typography'
-import { NameAddressWrapper, Gola } from './styles'
+import { NameAddressWrapper } from './styles'
+import { createIcon } from '@download/blockies'
 
 export default function NameAddress({
   imgSize,
   textSize,
   address = '0xa234bF5AcC3B150907704ce26D855A5638dEF890',
 }) {
+  const icon = createIcon({
+    // All options are optional
+    seed: address, // seed used to generate icon data, default: random
+
+    size: imgSize, // width/height of the icon in blocks, default: 10
+    scale: imgSize, // width/height of each block in pixels, default: 5
+  }).toDataURL()
+
   return (
     <NameAddressWrapper
       onClick={(event) => {
@@ -14,7 +23,13 @@ export default function NameAddress({
         window.open(`https://goerli.etherscan.io/address/${address}`)
       }}
     >
-      <Gola color="lightGrey" width={imgSize} />
+      <img
+        src={icon}
+        alt=""
+        height={imgSize}
+        width={imgSize}
+        style={{ borderRadius: '50%' }}
+      />
       <Typography type={textSize} color="lightGrey">
         {minifyAddress(address)}
       </Typography>
--- src/components/NameAddress/styles.js
+++ src/components/NameAddress/styles.js
@@ -2,14 +2,6 @@ import styled from 'styled-components'
 
 import { colors } from 'src/utils/colors'
 
-export const Gola = styled.div`
-  background-color: ${(props) => colors[props.color]};
-  border-radius: 100%;
-  width: ${(props) => `${props.width}px`};
-  height: ${(props) => `${props.width}px`};
-  display: flex;
-`
-
 export const NameAddressWrapper = styled.div`
   display: flex;
   align-items: center;
